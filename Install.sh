59ae05575e91930627c97b6c27b3acae30b3fc0e
pranavsk313 <122976840+pranavsk313@users.noreply.github.com>
2024-02-22 13:32:21 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,15 @@
 # To install docker on Ec2 instace 
-sudo apt-get update 
-sudo apt install docker.io
+sudo yum update all
+sudo yum remove docker \ 
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo systemctl start docker 
 sudo systemctl enable docker 
+
