01ac5739a9be078fe0ad6e9e0e3117e14191fe43
pranavsk313 <122976840+pranavsk313@users.noreply.github.com>
2024-02-22 13:29:30 +0530

    Update and rename Command.sh to Local_Command.sh

--- Local_Command.sh
+++ Local_Command.sh
@@ -1,14 +1,13 @@
-sudo apt-get update 
+sudo yum update all 
 mkdir projects 
 cd projects
-ls
 git clone https://github.com/shreys7/django-todo.git
-sudo apt-get update
-sudo apt install python3-pip
-pip install django
+
+sudo yum install pip git
+sudo yum install python3-pip
+sudo pip install django
 python3 manage.py migrate 
 python3 manage.py runserver 
 python3 manage.py runserver 0.0.0.0:8001
 nohup python3 manage.py runserver 0.0.0.0:8001 &
-lsof -i:8001
-kill -9 pid 
+sudo lsof -i:8001
